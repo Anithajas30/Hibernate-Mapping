6abf1f63fb55ba170a01bafc59fbb9aded1cf7db
ANITHA JASMINE <105966987+Anithajas30@users.noreply.github.com>
2024-07-05 13:04:23 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,40 @@
+package com.model;
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name="EMPDATA100")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="category" ,discriminatorType= DiscriminatorType.STRING)
+@DiscriminatorValue(value="general")
+
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	String name;
+	
+	
+	public Employee( String name) {
+		super();
+		this.name = name;
+	}
+	public Employee() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
